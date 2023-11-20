09b722a07b7d5d0f33126f3f43cc02263a9be36e
marine anglade <marine.anglade@epfedu.fr>
2023-11-20 10:34:25 +0100

    pions

---
+++ Projet_Onitama/src/projet_onitama/Pions.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projet_onitama;
+
+/**
+ *
+ * @author marin
+ */
+public class Pions {
+    int position_ligne;
+    int position_colonne; 
+    boolean eleve; 
+    
+    
+}
