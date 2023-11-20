7997791970ebf5a2ea1f4d342a07ee8a5139563c
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-11-20 10:34:20 +0100

    Création de la classe CaseGrille

---
+++ Projet_Onitama/src/projet_onitama/CaseGrille.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projet_onitama;
+
+/**
+ *
+ * @author nicol
+ */
+public class CaseGrille {
+    
+    private boolean etat;
+
+    public CaseGrille(boolean etat) {
+        this.etat = false;
+    }
+    
+    /**
+     * Passe l'etat de la case de false à true et inversement
+     */
+    public void activerCase() {
+        if (etat == false) {
+            etat = true;
+        } else {
+            etat = false;
+        }
+    }
+    
+    /**
+     * Renvoie true si la case est occupée, false si elle est inoccupée.
+     * @return
+     */
+    public boolean estOccupee() {
+        if (etat == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean getEtat() {
+        return etat;
+    }
+    
+    
+    
+}
