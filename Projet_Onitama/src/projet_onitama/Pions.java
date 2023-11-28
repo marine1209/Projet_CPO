a82f4aebaa56b4149d49e45212b464b16810fd86
marine anglade <marine.anglade@epfedu.fr>
2023-11-28 12:47:32 +0100

    Ajout d'un panel

--- Projet_Onitama/src/projet_onitama/Pions.java
+++ Projet_Onitama/src/projet_onitama/Pions.java
@@ -9,16 +9,33 @@ package projet_onitama;
  * @author marin
  */
 public class Pions {
+    boolean Sensei; 
     int position_ligne;
     int position_colonne; 
     boolean eleve;
-    String couleur;  //R pour rouge et B pour bleu
+    String couleur;  //R pour rouge et B pour
 
-    public Pions(int position_ligne, int position_colonne, boolean eleve) {
+    public Pions(int position_ligne, int position_colonne, boolean eleve, int nb_eleve, int nb_maitre) {
         this.position_ligne = position_ligne;
         this.position_colonne = position_colonne;
         this.eleve = eleve;
+  
     }
-    
-    
+
+    public boolean get_eleve() {
+        return eleve;
+    }
+
+    public int getPosition_ligne() {
+        return position_ligne;
+    }
+
+    public int getPosition_colonne() {
+        return position_colonne;
+    }
+   
+    public boolean getSensei(){
+        return Sensei; 
+    }
+
 }
